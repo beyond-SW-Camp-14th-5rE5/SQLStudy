c1fe3c10690d2cb5003c6cdac70c626a5a4f3c03
50071 <rkd50071@gmail.com>
2025-01-07 19:51:22 +0900

    Revert "[SJ] 131112, 131120, 132203, 133024, 133025, 151136"
    This reverts commit f7e6a27925e3a0da6905a0c08c6c78e1aad9e0ca.

--- SELECT/132203) 흉부외과 또는/수지.sql
+++
@@ -1,4 +0,0 @@
-SELECT dr_name, dr_id, mcdp_cd, date_format(hire_ymd, '%Y-%m-%d') hire_ymd
-from doctor
-where mcdp_cd in ('CS', 'GS')
-order by hire_ymd desc, dr_name asc
--- SELECT/133025) 과일로 만든/수지.sql
+++
@@ -1,6 +0,0 @@
-select a.flavor
-from first_half a
-join icecream_info b using (flavor)
-where a.total_order > 3000
-and b.ingredient_type like 'fruit%'
-order by total_order desc
